42ee6352be38e5fbc1868c6eff3abbac80b96630
Niall <noccer@gmail.com>
2016-07-01 15:17:53 +1000

    NOC: preliminary pre-save code inserted. commented out for the moment.

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var timeController = require('../controllers/timeController');
 
 // Create the CustomerSchema.
 var RestaurantSchema = new mongoose.Schema({
@@ -60,7 +61,17 @@ var RestaurantSchema = new mongoose.Schema({
   ]
 });
 
-// Restaurant.pre.save function here that will work with the finishedWaiting time
+// Restaurant.pre.save function to take startedWaiting time
+// and use it to create finishedWaiting time using the 'ETA' time value
+// i.e. Add a customer at '15:10', eta time is 15mins, therefore finishedWaiting will be '15:25'
+// RestaurantSchema.pre('save', function(next) {
+//   var now = new Date();
+//   this.updated_at = now;
+//   if (!this.created_at) {
+//     this.created_at = now;
+//   }
+//   next(); //must call this
+// });
 
 // Export the model.
 module.exports = mongoose.model('restaurant', RestaurantSchema);
