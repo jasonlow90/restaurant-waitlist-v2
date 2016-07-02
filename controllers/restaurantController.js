5729adc98db70003479544007d76a1c49bdef02a
Niall <noccer@gmail.com>
2016-07-02 14:02:50 +1000

    NOC: tidy up index.js file down the bottom. removing empty lines etc.

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -11,7 +11,6 @@ function showRestaurants(req, res){
 }
 
 function getAllCustomers(req, res){
-
   Restaurant.findOne({restaurantName: "Macdonald"}, function(err, restaurant){
     if(err) res.status(402).json({message: err.errmsg});
     res.status(200).json(restaurant.customers);
@@ -19,11 +18,9 @@ function getAllCustomers(req, res){
 }
 
 function showCustomer(req, res){
-
   function findCustomer(customers){
     return customers.customerName === req.params.customer;
   }
-
   Restaurant.findOne({restaurantName: "Macdonald"}, function(err, restaurant){
     if(err) res.status(402).json({message: err.errmsg});
     res.status(200).json(restaurant.customers.find(findCustomer));
@@ -31,9 +28,7 @@ function showCustomer(req, res){
 }
 
 function addRestaurant(req, res){
-
   var restaurantParams = req.body;
-
   Restaurant.create(restaurantParams , function(err, restaurant){
     if(err) res.status(401).json({message: "Hello"});
     res.json(restaurant);
@@ -42,13 +37,11 @@ function addRestaurant(req, res){
 }
 
 function addCustomer(req, res){
-
-
-
-  Restaurant.findOne({_id: "577606322f016641459de0e0" } , function(err, restaurant){
+  // Restaurant.findOne({_id: "577606322f016641459de0e0" } , function(err, restaurant){
+  Restaurant.findOne({_id: "57771ea2c31b681085727b6c" } , function(err, restaurant){ //niall seed
     if(err) res.status(401).json({message: "Hello"});
     Customer.create({
-      customerName: "Peter",
+      customerName: "NIALL",
       phone: "0430852260",
       isVip: true,
       heads: 3,
@@ -57,7 +50,8 @@ function addCustomer(req, res){
     }, function(err, customer){
       if(err) res.status(400).json({message: "FUCK!"});
       // res.json(customer);
-      Restaurant.findOneAndUpdate({_id: "577606322f016641459de0e0"},{$push: {customers: customer}}, function(err, rest){
+      // Restaurant.findOneAndUpdate({_id: "577606322f016641459de0e0"},{$push: {customers: customer}}, function(err, rest){
+      Restaurant.findOneAndUpdate({_id: "57771ea2c31b681085727b6c"},{$push: {customers: customer}},  function(err, rest){ //niall seed
         if(err) res.status(400).json({message: "FUCK ME"});
         res.status(202).json(rest);
       });
@@ -69,26 +63,19 @@ function addCustomer(req, res){
 function populate(req, res){
 //   Customer.findOne({customerName: "Jason"}).populate('_restaurant', [])
 // }
-
-
-
-  Restaurant.findOne({_id: "577606322f016641459de0e0" }).populate('customers').exec(function (err, customer){
+  Restaurant.find({}).populate('customers').exec(function (err, customer){
     if(err) res.json({message: "fuck me?"});
     res.json(customer);
   });
-
 }
 
 function removeCustomer(req,res){
-
-
   var customerParams = req.body;
   Restaurant.findOneAndUpdate({restaurantName: 'Macdonald'},{$pull: {customers: customerParams}} , function(err, restaurant){
     if(err) res.status(401).json({message: err.errmsg});
     if(!restaurant) res.status(401).json({message: "No such customer"});
     res.json("Success Removed!");
   });
-
 }
 
   // Keeping update code for reference:
