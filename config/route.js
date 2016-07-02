3e94acb4ef908358123a695be9f43527c0dbe02f
Niall <noccer@gmail.com>
2016-07-03 01:20:16 +1000

    NOC: overhauled the routes. new URL's set up as well as detailed descriptions of what each one does.

--- config/route.js
+++ config/route.js
@@ -10,17 +10,29 @@ var usersController = require('../controllers/UserController');
 // router.use('/add', expressJWT({secret: secret}));
 // router.use('/remove', expressJWT({secret: secret}));
 
-// index
-router.get('/', restaurantController.showRestaurants);
-// customers
-router.get('/customers', restaurantController.getAllCustomers);
-router.get('/customers/customer/:id', restaurantController.showCustomer);
-// restaurants
-router.post('/add', restaurantController.addCustomer);
+// ----------index----------
+router.get('/', restaurantController.showRestaurants); // show all restaurants
+
+// ----------customers----------
+router.get('/:restaurantSuburb', restaurantController.getAllCustomers); // view list of customers waiting
+router.get('/:restaurantSuburb/:phone', restaurantController.getAllCustomers); // view list of customers waiting, but with the customer whose phone number is in url shown highlighted.
+router.get('/:restaurantSuburb/addCustomer', restaurantController.addCustomer); // render the add customer form
+router.post('/:restaurantSuburb/addCustomer', restaurantController.addCustomer); // submit customer to the database
+router.post('/:restaurantSuburb/removeCustomer', restaurantController.removeCustomer); // customer has been seated or canceled their 'order'
+router.get('/:restaurantSuburb/updateCustomer', restaurantController.updateCustomer); // render form - update (should also include a delete link for convenience)
+router.post('/:restaurantSuburb/updateCustomer', restaurantController.updateCustomer); // submit customer updates to database
+
+// ----------restaurants---------- admin pages only, not viewable to public
+router.get('/restaurant/add', restaurantController.addRestaurant); // render form - setting up intitial restaurant
+router.post('/:restaurantSuburb/add', restaurantController.addRestaurant); // submit restaurant to the database
+router.get('/:restaurantSuburb/updateRestaurant', restaurantController.updateRestaurant); // render form - edit restaurant
+router.post('/:restaurantSuburb/updateRestaurant', restaurantController.updateRestaurant); // submit restaurant to the database
+
+// ----------testing routes----------
+router.get('/customer/:id', restaurantController.showCustomer);
 router.get('/populate', restaurantController.populate);
-router.post('/restaurant/add', restaurantController.addRestaurant);
-router.post('/remove', restaurantController.removeCustomer);
-// users
-router.post('/signin', usersController.authenticateUser);
+
+// ----------users---------- =>DEPRECATED
+// router.post('/signin', usersController.authenticateUser); =>DEPRECATED
 
 module.exports = router;
