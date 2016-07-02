6f1f230463365489c04fb0c60b47c1bc5c43f88e
Niall <noccer@gmail.com>
2016-07-02 14:22:04 +1000

    NOC: tidy up the routes.js file a little to get the routes organised into groups.

--- config/route.js
+++ config/route.js
@@ -4,34 +4,23 @@ var expressJWT = require('express-jwt');
 var jwt = require('jsonwebtoken');
 var secret = "waiting_list";
 
-
 var restaurantController = require('../controllers/restaurantController');
 var usersController = require('../controllers/UserController');
 
 // router.use('/add', expressJWT({secret: secret}));
 // router.use('/remove', expressJWT({secret: secret}));
 
-
-
+// index
 router.get('/', restaurantController.showRestaurants);
-
+// customers
 router.get('/customers', restaurantController.getAllCustomers);
-
 router.get('/customers/customer/:id', restaurantController.showCustomer);
-
-
+// restaurants
 router.post('/add', restaurantController.addCustomer);
-
 router.get('/populate', restaurantController.populate);
-
-
 router.post('/restaurant/add', restaurantController.addRestaurant);
-
-
 router.post('/remove', restaurantController.removeCustomer);
-
-
-
+// users
 router.post('/signin', usersController.authenticateUser);
 
 module.exports = router;
