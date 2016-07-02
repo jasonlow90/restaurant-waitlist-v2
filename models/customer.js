505b3d38c6848fd81da89f60d9ead577c14a3f54
Niall <noccer@gmail.com>
2016-07-02 12:26:44 +1000

    NOC: added in some references to the timerController.js file so that the model could find it. Added in some middleware to perform some action to finishedWaiting field before it saves to the database.

--- models/customer.js
+++ models/customer.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 var Restaurant = require('./restaurant');
+var timeController = require('../controllers/timeController');
+var moment = require('moment');
 
 // Create the CustomerSchema.
 var CustomerSchema = new mongoose.Schema({
@@ -36,6 +38,26 @@ var CustomerSchema = new mongoose.Schema({
       type: Date,
       // required: true
    }
+});
+
+// User middleware
+CustomerSchema.pre('save', function(next) {
+  //           ⤹ the customer
+  // var customer = this;
+
+  timeController.resetTimeOfETA(CustomerSchema.eta);
+  Customer.finishedWaiting = timeController.timeETA;
+
+  // if(!user.isModified('password')) return next();
+  // bcrypt.genSalt(10, function(err, salt) {
+  //   if (err) return next(err);
+  //   // 3 arguments: password, salt, hash
+  //   bcrypt.hash(user.password, salt, function(err, hash) {
+  //     if (err) return next(err);
+  //     user.password = hash;
+  //     next();
+  //   });
+  // });
 });
 
 // Export the model.
