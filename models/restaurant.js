29252dc32632d31440f9fecf86dc85f1f8db6f74
Niall <noccer@gmail.com>
2016-07-04 11:32:37 +1000

    NOC: added in middleware to generate a restaurantNameSuburb string

--- models/restaurant.js
+++ models/restaurant.js
@@ -4,82 +4,77 @@ var Customer = require('./customer');
 
 // Create the CustomerSchema.
 var Restaurant = new mongoose.Schema({
-   restaurantName: {
-      type: String,
-      required: true,
-      unique: true
-   },
-   website: {
-      type: String,
-      required: true
-   },
-   phone: {
-      type: String,
-      required: true
-   },
-   address: {
-      type: String,
-      required: true
-   },
-   suburb: {
-      type: String,
-      required: true
-   },
-   restaurantNameSuburb: {
-     type: String
-   },
-   postcode: {
-      type: String,
-      required: true
-   },
-   cuisine: {
-     type: String
-   },
-   username: {
-     type: String,
-     required: true,
-     unique: true,
-     select: false
-   },
-   password: {
-     type: String,
-     required: true,
-     select: false
-   },
-   restaurantEmail: {
-     type: String,
-     unique: true,
-     required: true,
-     select: false
-   },
-   customers : [{
-     type: mongoose.Schema.ObjectId,
-     ref: 'Customer'
-   }
- ]
+  restaurantName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  website: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  suburb: {
+    type: String,
+    required: true
+  },
+  restaurantNameSuburb: {
+    type: String
+  },
+  postcode: {
+    type: String,
+    required: true
+  },
+  cuisine: {
+    type: String
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    select: false
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  },
+  restaurantEmail: {
+    type: String,
+    unique: true,
+    required: true,
+    select: false
+  },
+  customers: [{
+    type: mongoose.Schema.ObjectId,
+    ref: 'Customer'
+  }]
 });
 
-// Restaurant.pre('save', function(next) {
-//   var retaurant = this;
-//   // NOC: pseudo code
-//   // take the restaurantName value, add a space, then add on the suburb name.
-//   var restaurantName = restaurant.restaurantName;
-//   restaurantName = restaurantName.toLowerCase().replace(/[&\/\\#,+()$~%.'":*?<>{}]/g,'-');
-//
-//   var suburb = restaurant.suburb;
-//   // lowercase the whole thing
-//   // remove anything except letters and numbers
-//   // replace spaces with dashes
-//   // insert final string into restaurantNameSuburb
-//   next();
-//   // **********
-//   // var customer = this;
-//   // var now = new Date();
-//   // var timeETA = moment(now);
-//   // timeETA = timeETA.add(customer.eta, 'minutes');
-//   // this.finishedWaiting = timeETA;
-//   // next();
-// });
+Restaurant.pre('save', function(next) {
+  // take the restaurantName value, add a space, then add on the suburb name.
+  var restaurant = this;
+  var restaurantName = restaurant.restaurantName;
+  restaurantName = restaurantName.toLowerCase()
+    .replace(/[&\/\\#,+()$~%."@:*?<>{}]/g, '-') //replace odd characters with a dash
+    .replace(' ', '-') //replace spaces with a dash
+    .replace(/'/g,''); //remove apostrophe
+  var suburb = restaurant.suburb.toLowerCase()
+    .replace(/[&\/\\#,+()$~%."@:*?<>{}]/g, '-')
+    .replace(' ', '-') //replace spaces with a dash
+    .replace(/'/g,''); //remove apostrophe
+  var restaurantNameSuburb = restaurantName + "-" + suburb; //concatenate
+  // insert final string into restaurantNameSuburb
+  restaurant.restaurantNameSuburb = restaurantNameSuburb; //insert into the mongo record!
+  next();
+});
 
 // Let's encrypt our passwords using only the model!
 // This is a hook, a function that runs just before you save.
