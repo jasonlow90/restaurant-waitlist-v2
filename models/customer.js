3d2efa51a6c39ff9a6e57d148ec9c32aa7c9f19c
Jason Low <lowjiashen@gmail.com>
2016-07-04 12:50:33 +1000

    Solved conflict issues in restaurant model

--- models/customer.js
+++ models/customer.js
@@ -11,7 +11,8 @@ var Customer = new mongoose.Schema({
    },
    customerName: {
       type: String,
-      required: true
+      required: true,
+      unique: true
    },
    phone: {
       type: String,
--- models/restaurant.js
+++ models/restaurant.js
@@ -1,61 +1,63 @@
 var mongoose = require('mongoose');
 var timeController = require('../controllers/timeController');
 var Customer = require('./customer');
+var bcrypt = require('bcrypt');
 
 // Create the CustomerSchema.
 var Restaurant = new mongoose.Schema({
-  restaurantName: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  website: {
-    type: String,
-    required: true
-  },
-  phone: {
-    type: String,
-    required: true
-  },
-  address: {
-    type: String,
-    required: true
-  },
-  suburb: {
-    type: String,
-    required: true
-  },
-  restaurantNameSuburb: {
-    type: String
-  },
-  postcode: {
-    type: String,
-    required: true
-  },
-  cuisine: {
-    type: String
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    select: false
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false
-  },
-  restaurantEmail: {
-    type: String,
-    unique: true,
-    required: true,
-    select: false
-  },
-  customers: [{
-    type: mongoose.Schema.ObjectId,
-    ref: 'Customer'
-  }]
+   restaurantName: {
+      type: String,
+      required: true,
+      unique: true
+   },
+   website: {
+      type: String,
+      required: true
+   },
+   phone: {
+      type: String,
+      required: true
+   },
+   address: {
+      type: String,
+      required: true
+   },
+   suburb: {
+      type: String,
+      required: true
+   },
+   restaurantNameSuburb: {
+     type: String
+   },
+   postcode: {
+      type: String,
+      required: true
+   },
+   cuisine: {
+     type: String
+   },
+   username: {
+     type: String,
+     required: true,
+     unique: true,
+     select: false
+   },
+   password: {
+     type: String,
+     required: true
+    //  select: false
+   },
+   restaurantEmail: {
+     type: String,
+     unique: true,
+     required: true
+    //  select: false
+   },
+   customers : [{
+     type: mongoose.Schema.ObjectId,
+     ref: 'Customer'
+   }
+ ]
 });
 
 Restaurant.pre('save', function(next) {
@@ -73,29 +75,25 @@ Restaurant.pre('save', function(next) {
   var restaurantNameSuburb = restaurantName + "-" + suburb; //concatenate
   // insert final string into restaurantNameSuburb
   restaurant.restaurantNameSuburb = restaurantNameSuburb; //insert into the mongo record!
-  next();
-});
 
 // Let's encrypt our passwords using only the model!
 // This is a hook, a function that runs just before you save.
-// Restaurant.pre('save', function(next) {
-//     var restaurant = this;
-//
-//     // only hash the name if it has been modified (or is new)
-//     if (!restaurant.isModified('password')) return next();
-//
-//     // just for example purposes, let's keep the agent's name in a separate field
-//    //  user.unencryptedName = agent.name;
-//     // bcrypt can come up with a salt for us (just pass it a number)
-//     restaurant.password = bcrypt.hashSync(restaurant.password, 10);
-//
-//     next();
-// });
-//
-// Restaurant.methods.authenticate = function(candidatePassword, callback){
-//   //invoke bcrypt to attempt to compare our user with the stored user pasword
-//   bcrypt.compare(candidatePassword, this.password, callback);
-// };
+
+    // only hash the name if it has been modified (or is new)
+    if (!restaurant.isModified('password')) return next();
+
+    // just for example purposes, let's keep the agent's name in a separate field
+   //  user.unencryptedName = agent.name;
+    // bcrypt can come up with a salt for us (just pass it a number)
+    restaurant.password = bcrypt.hashSync(restaurant.password, 10);
+
+    next();
+});
+
+Restaurant.methods.authenticate = function(candidatePassword, callback){
+  //invoke bcrypt to attempt to compare our user with the stored user pasword
+  bcrypt.compare(candidatePassword, this.password, callback);
+};
 
 // Export the model.
 module.exports = mongoose.model('Restaurant', Restaurant);
